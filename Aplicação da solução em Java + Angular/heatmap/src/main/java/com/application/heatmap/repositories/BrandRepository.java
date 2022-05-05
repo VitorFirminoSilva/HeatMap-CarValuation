0d21aec22f4599273b977a94f8d548a137c75a59
VitorFirminoSilva <vitorfirmino70@gmail.com>
2022-05-05 20:25:23 -0300

    Add Outhers Classes

---
+++ Aplicação da solução em Java + Angular/heatmap/src/main/java/com/application/heatmap/repositories/BrandRepository.java
@@ -0,0 +1,10 @@
+package com.application.heatmap.repositories;
+
+import com.application.heatmap.entities.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Long>{
+    
+}
---
+++ Aplicação da solução em Java + Angular/heatmap/src/main/java/com/application/heatmap/repositories/ValuationRepository.java
@@ -0,0 +1,10 @@
+package com.application.heatmap.repositories;
+
+import com.application.heatmap.entities.Valuation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ValuationRepository extends JpaRepository<Valuation, Long>{
+    
+}
